8b13eaaae613e4fe843366c62cd36790a446a581
Caio Monte <caiomm076@gmail.com>
2025-06-19 13:40:38 -0300

    estrutura básica testada

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("form-cadastro").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+
+  fetch("http://localhost:5000/usuarios", { // depois troca esse URL pro backend real
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ nome, email })
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("mensagem").innerText = data.mensagem;
+    document.getElementById("form-cadastro").reset();
+  })
+  .catch(err => {
+    document.getElementById("mensagem").innerText = "Erro ao enviar.";
+    console.error(err);
+  });
+});
